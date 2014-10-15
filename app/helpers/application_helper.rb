b85016fed7cc0c00759fb8ec74c201297ec0a8df
Garth Fasano <garthfasano@gmail.com>
2014-10-15 17:31:51 +0000

    Finish static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  #returns page title dynamicly
+  def full_title(page_title)
+    base_title = "My Blog"
+    if page_title.empty?
+      base_title
+    else "#{base_title} | #{page_title}"
+    end
+  end
 end
